eff9178069110a1d9153097b17fb2f6b64625c5a
maxarvid <max.arvid.anderson@gmail.com>
2021-09-07 14:38:03 +0200

    adds components to About.jsx and Projects.jsx

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const About = () => {
+  return;
+  <Container>
+    <h1 id="about-header">About Me</h1>
+    <p>
+      TKTKTKTKTKTKT To be added. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Mollitia harum quaerat quo, explicabo sed amet quam
+      maxime. Exercitationem illum at ex excepturi officia omnis rem cupiditate
+      iusto, quae aspernatur. Dolorem!
+    </p>
+  </Container>;
+};
+
+export default About;
---
+++ src/Projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Projects = () => {
+  return (
+    <Container>
+      <h1 id="projects-header">My Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        cum obcaecati. Hic facilis nesciunt atque repellat nobis enim at,
+        aliquam, tempora laudantium fuga error. Sapiente sed provident earum
+        quas ipsum.
+      </p>
+    </Container>
+  );
+};
+
+export default Projects;
