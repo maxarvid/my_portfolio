39bea0593222dd5dc540cda994ae70a27c61c335
maxarvid <max.arvid.anderson@gmail.com>
2021-09-09 18:30:14 +0200

    refactors Contact.jsx into functional component to play nice with semantic-react Form objects

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,73 +1,152 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Form } from "semantic-ui-react";
 
-const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-};
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-class ContactForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "", email: "", message: "" };
-  }
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  handleSubmit = (e) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state }),
-    })
-      .then(() => alert("Success!"))
-      .catch((error) => alert(error));
+  const handleSubmit = (e) => {
+    setIsSubmitted(true);
     e.preventDefault();
   };
 
-  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "+" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
 
-  render() {
-    const { name, email, message } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit} netlify name="contact">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>
-            Your Name:
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email:
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <textarea
-              name="message"
-              value={message}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    );
-  }
-}
+  useEffect(() => {
+    if (isSubmitted) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact-form", ...formData }),
+      })
+        .then(() => alert("Success!"))
+        .then(() => setIsSubmitted(false))
+        .then(() => setFormData({ name: "", email: "", message: "" }))
+        .catch((error) => alert(error));
+    }
+  }, [formData, isSubmitted]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+          <label>Your Name</label>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={formData.name}
+            placeholder="Your name"
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Your Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Your email"
+          />
+        </Form.Field>
+        <Form.TextArea
+          label="Message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          placeholder="What's on your mind?"
+        />
+        <Form.Button type="submit">Send</Form.Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default ContactForm;
+
+// import React, { Component } from "react";
+// import { Container, Form, Button, TextArea } from "semantic-ui-react";
+
+// const encode = (data) => {
+//   return Object.keys(data)
+//     .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//     .join("&");
+// };
+
+// class ContactForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { name: "", email: "", message: "" };
+//     debugger;
+//   }
+
+//   handleSubmit = (e) => {
+//     fetch("/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: encode({ "form-name": "contact", ...this.state }),
+//     })
+//       .then(() => alert("Success!"))
+//       .catch((error) => alert(error));
+//     e.preventDefault();
+//   };
+
+//   handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+//   render() {
+//     const { name, email, message } = this.state;
+//     return (
+//       <Container>
+//         <Form onSubmit={this.handleSubmit} netlify name="contact">
+//           <input type="hidden" name="form-name" value="contact" />
+//           <Form.Field>
+//             <label>Your Name</label>
+//             <input
+//               type="text"
+//               name="name"
+//               value={name}
+//               onChange={this.handleChange}
+//               placeholder="Your name"
+//             />
+//           </Form.Field>
+//           <Form.Field>
+//             <label>Your Email</label>
+//             <input
+//               type="email"
+//               name="email"
+//               value={email}
+//               onChange={this.handleChange}
+//               placeholder="name@example.com"
+//             />
+//           </Form.Field>
+//           <Form.Field
+//             label="Message"
+//             control={TextArea}
+//             placeholder="What's on your mind?"
+//             value={message}
+//             onChange={this.handleChange}
+//           />
+//           <Button type="submit">Send</Button>
+//         </Form>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default ContactForm;
