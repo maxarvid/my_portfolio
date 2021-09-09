47a9096ce219a8df805a0bbeb97c55c96de60b85
maxarvid <max.arvid.anderson@gmail.com>
2021-09-09 09:45:55 +0200

    adds tests for finding alt attr

---
+++ cypress/integration/userCanSeeAltImageTextInProjectList.feature.js
@@ -0,0 +1,24 @@
+describe("User can access the alt for images in project list", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get("#projects-tab").click();
+  });
+
+  it("it is expected to have an alt for the first project", () => {
+    cy.get("#project-1").within(() => {
+      cy.get(".image").should('have.attr', 'alt')
+    });
+  });
+
+  it("it is expected to have an alt for the second project", () => {
+    cy.get("#project-2").within(() => {
+      cy.get(".image").should('have.attr', 'alt')
+    });
+  });
+
+  it("it is expected to have an alt for the third project", () => {
+    cy.get("#project-3").within(() => {
+      cy.get(".image").should('have.attr', 'alt')
+    });
+  });
+});
