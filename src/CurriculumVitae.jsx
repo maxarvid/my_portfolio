e38849c8193e4275bf59d55982260bc679dff1c2
maxarvid <max.arvid.anderson@gmail.com>
2021-09-11 16:40:54 +0200

    adds axios loading in of cv.json and non-styled display of data on cv-tab

--- src/CurriculumVitae.jsx
+++ src/CurriculumVitae.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import axios from 'axios'
-import { Container } from 'semantic-ui-react'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, List } from "semantic-ui-react";
 
 const CurriculumVitae = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get("./data/cv.json").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
+  let itemList = items.map((item) => {
+    return (
+      <div id={`item-${item.id}`} key={item.id}>
+        <h3 id="item-name">{item.name}</h3>
+        <h3 id="item-range">{item.range}</h3>
+        <p id="item-description">{item.description}</p>
+      </div>
+    );
+  });
   return (
     <Container>
-      
+      <List>{itemList}</List>
     </Container>
-  )
-}
+  );
+};
 
-export default CurriculumVitae
+export default CurriculumVitae;
