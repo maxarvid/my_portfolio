feecc4d153194e0506c7977e356f2356169c7682
maxarvid <max.arvid.anderson@gmail.com>
2021-09-12 12:06:21 +0200

    resintalls yarn and adds About.jsx test

--- src/specs/About.test.jsx
+++ src/specs/About.test.jsx
@@ -1,2 +1,20 @@
 import React from "react";
-import About from "../About"
+import About from "../About";
+import { render, screen } from "@testing-library/react";
+
+describe("About.jsx", () => {
+  beforeEach(() => {
+    render(<About />);
+  });
+
+  it("is currentlty expected to contain lorem ipsum", () => {
+    expect(
+      screen.getByText(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        {
+          exact: false,
+        }
+      )
+    ).toBeInTheDocument();
+  });
+});
